580eb101f92ed0ec37bd7758c7d7d3169c1a9275
richa <richa@RICHARDHP>
2024-10-22 00:51:58 -0500

    a dormir

--- hackaton react/hackaton/src/app/page.js
+++ hackaton react/hackaton/src/app/page.js
@@ -1,11 +1,11 @@
 import Categories from "@/components/Categories";
+import Navbar from "@/components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ThemeProvider } from 'react-bootstrap';
-
 
 const Home = () => {
   return (
     <div>
+     
       <Categories />
     </div>
   );
--- hackaton react/hackaton/src/pages/culture/[culture].js
+++ hackaton react/hackaton/src/pages/culture/[culture].js
@@ -1,35 +1,27 @@
+import React from 'react';
 import { useRouter } from 'next/router';
 import listaDeProductos from '@/components/lista/listaDeProductos';
-import Product from '@/components/Product';
+
 
 const Cultura = () => {
   const router = useRouter();
   const { culture } = router.query;
 
-  const productosFiltrados = listaDeProductos.filter(producto => producto.categoria === culture);
+  const culturaClickeada = listaDeProductos.find((producto) => producto.categoria === culture);
+
+  if (!culturaClickeada) {
+    return <p>No se encontró ningún producto en esta categoría.</p>;
+  }
 
   return (
     <div>
       <h2>Productos de la categoría: {culture}</h2>
-      {productosFiltrados.length > 0 ? (
-        <div >
-          {productosFiltrados.map((producto, index) => (
-            <Product 
-              key={index}
-              nombre={producto.nombre}
-              descripcion={producto.descripcion}
-              precio={producto.precio}
-              comunidad={producto.comunidad}
-              categoria={producto.categoria}
-              disponibilidad={producto.disponibilidad}
-            />
-          ))}
-        </div>
-      ) : (
-        <p>No hay productos disponibles en esta categoría.</p>
-      )}
+      <div>
+        <p>{culturaClickeada.descripcionCultura}</p>
+        <img src={culturaClickeada.imagenCultura} />
+      </div>
     </div>
-  );
+  ); 
 };
 
 export default Cultura;
